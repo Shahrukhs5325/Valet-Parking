9b46972322135185b744bc0ab3e14d4c311b7ece
Shahrukh <shahrukh@ezygentechnology.com>
2024-08-28 12:52:37 +0530

    font added

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Amplify } from 'aws-amplify';
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { PaperProvider } from 'react-native-paper';
-import Navigation from './src/navigation/StackNavigation';
-import { Amplify } from 'aws-amplify';
+import { MD2LightTheme, PaperProvider, configureFonts } from 'react-native-paper';
 import { UserContextProvider } from './src/context/user/UserContext';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Navigation from './src/navigation/StackNavigation';
+import { fontConfig } from './src/theme/fonts';
 
-const config = {
+const cognitoConfig = {
   identityPoolId: '',
   region: "me-south-1",
   userPoolId: "me-south-1_tgPFCMubu",
@@ -15,7 +16,7 @@ const config = {
 
 
 function App(): React.JSX.Element {
-  Amplify.configure(config);
+  Amplify.configure(cognitoConfig);
 
   const queryClient = new QueryClient({
     defaultOptions: {
@@ -26,11 +27,16 @@ function App(): React.JSX.Element {
     },
   });
 
+  const theme = {
+    ...MD2LightTheme,
+    fonts: configureFonts({ config: fontConfig, isV3: false }),
+  };
+
   return (
     <>
       <QueryClientProvider client={queryClient}>
         <UserContextProvider>
-          <PaperProvider>
+          <PaperProvider theme={theme}>
             <Navigation />
           </PaperProvider>
         </UserContextProvider>
--- src/theme/fonts.ts
+++ src/theme/fonts.ts
@@ -1,28 +1,47 @@
 export const FONT = {
-
-    // bold: "",
-    // regular: "",
-
     // Inter
     regular: "Inter-Regular",
     thin: "Inter-Thin",
     bold: "Inter-Bold",
     semiBold: "Inter-SemiBold",
     extraBold: "Inter-ExtraBold",
-
-
-    // FuzzyBubbles
-    // bold: "FuzzyBubbles Bold",
-    // regular: "FuzzyBubbles Regular",
 };
 
-export const FONT_AR = {
-    
-    // Alexandria for Arabic
-
-    regular: "Alexandria-Regular",
-    thin: "Alexandria-Thin",
-    bold: "Alexandria-Bold",
-    semiBold: "Alexandria-SemiBold",
-    extraBold: "Alexandria-ExtraBold",
+export const fontConfig = {
+    ios: {
+        regular: {
+            fontFamily: 'sans-serif',
+            fontWeight: 'normal',
+        },
+        medium: {
+            fontFamily: 'sans-serif-medium',
+            fontWeight: 'normal',
+        },
+        light: {
+            fontFamily: 'sans-serif-light',
+            fontWeight: 'normal',
+        },
+        thin: {
+            fontFamily: 'sans-serif-thin',
+            fontWeight: 'normal',
+        },
+    },
+    android: {
+        regular: {
+            fontFamily: 'sans-serif',
+            fontWeight: 'normal',
+        },
+        medium: {
+            fontFamily: 'sans-serif-medium',
+            fontWeight: 'normal',
+        },
+        light: {
+            fontFamily: 'sans-serif-light',
+            fontWeight: 'normal',
+        },
+        thin: {
+            fontFamily: 'sans-serif-thin',
+            fontWeight: 'normal',
+        },
+    }
 };
